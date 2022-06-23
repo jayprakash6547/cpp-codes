50ac7e7474e2403d2f102cb23efc6d10f532630d
jayprakash6547 <87560854+jayprakash6547@users.noreply.github.com>
2022-06-23 13:48:20 +0530

    Add files via upload

---
+++ theLeadGame.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int n;
+ cin>>n;
+ int sum1=0,sum2=0;
+ int winner,lead=0;
+ int diff=0;
+ for(int i=0;i<n;i++){
+ 	int p1,p2;
+ 	cin>>p1>>p2;
+ 	sum1+=p1;
+ 	sum2+=p2;
+ 	if(sum1>sum2){
+ 		diff=sum1-sum2;
+ 		if(diff>lead){
+ 			lead=diff;
+ 			winner=1;
+ 		}
+ 	}
+ 	else{
+ 		diff=sum2-sum1;
+ 		if(diff>lead){
+ 			lead-diff;
+ 			winner=2;
+ 		}
+ 	}
+ } 
+ cout<<winner<<" "<<lead;
+}
